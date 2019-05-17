aa61f20885b3a5884e95fb7067526ad9c7ba02a2
Rahul Choudhary <rahul@localhost.localdomain>
2019-05-17 22:30:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React : By - Rahul Choudhary
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,10 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without crashing : Test 2', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
